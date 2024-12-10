958ff7b0277f39121413666328eec0302835d437
yurim <y1551463@gmail.com>
2024-12-10 22:57:25 +0900

    feat : user entity 생성

---
+++ auth-service/src/main/java/com/logistics/platform/auth_service/domain/model/Role.java
@@ -0,0 +1,5 @@
+package com.logistics.platform.auth_service.domain.model;
+
+public enum Role {
+    MASTER, HUB_MANAGER, DELIVERY_MANAGER, COMPANT_MANAGER;
+}
---
+++ auth-service/src/main/java/com/logistics/platform/auth_service/domain/model/User.java
@@ -0,0 +1,48 @@
+package com.logistics.platform.auth_service.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "p_user")
+@Entity
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    private String slackId;
+
+    @Column(nullable = false)
+    private String number;
+
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean isDeleted;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
