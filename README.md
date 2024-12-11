c6d4ab110584d9ea5d101cb4077e3d5ce1060422
hiimsajo <hiimsajo@gmail.com>
2024-12-11 11:45:26 +0900

    chore : 배송담당자 git ignore

--- README.md
+++
@@ -1,1 +0,0 @@
-![image](https://github.com/user-attachments/assets/e009b709-2a67-41f9-b55c-f04e502a735f)
--- company-service/src/main/java/com/logistics/platform/company_service/presentation/global/ex/CustomApiException.java
+++
@@ -1,7 +0,0 @@
-package com.logistics.platform.company_service.presentation.global.ex;
-
-public class CustomApiException extends RuntimeException{
-  public CustomApiException(String message) {
-    super(message);
-  }
-}
