b1c9dc60c23e953beb79c1f1e4a0b116bd8b47ff
hiimsajo <hiimsajo@gmail.com>
2024-12-11 14:32:38 +0900

    Re-add files after cache clear

---
+++ auth-service/src/test/java/com/logistics/platform/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.platform.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
