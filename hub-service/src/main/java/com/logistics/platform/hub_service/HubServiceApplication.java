b41b9c279250567bcbd700e72606415ff2b21dfd
hiimsajo <hiimsajo@gmail.com>
2024-12-11 14:32:38 +0900

    Re-add files after cache clear

---
+++ hub-service/src/main/java/com/logistics/platform/hub_service/HubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.platform.hub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubServiceApplication.class, args);
+	}
+
+}
