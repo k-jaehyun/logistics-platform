b8c7fdb72bf4ebeee0aa0eed408d58cc9f81e39d
hiimsajo <hiimsajo@gmail.com>
2024-12-12 06:30:38 +0900

    ✨ feat : 배송담당자 Exception, responseDto 파일 추가

---
+++ delivery-manager-service/src/main/java/com/logistics/platform/deliverymanagerservice/presentation/global/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.logistics.platform.deliverymanagerservice.presentation.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResponseDto<T> {
+
+  public static final int SUCCESS = 1;
+  public static final int FAILURE = -1;
+  private final Integer code; // 1 성공, -1 실패
+  private final String msg;
+  private final T data;
+
+  public ResponseDto(Integer code, String msg) {
+    this.code = code;
+    this.msg = msg;
+    data = null;
+  }
+
+}
