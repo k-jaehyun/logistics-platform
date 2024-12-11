332a0acedbbb007f3427f0c3f75f80c73b0a3fe1
yurim <y1551463@gmail.com>
2024-12-11 17:31:04 +0900

    feat : validation exception 추가

--- auth-service/src/main/java/com/logistics/platform/auth_service/common/exception/CustomExceptionHandler.java
+++ auth-service/src/main/java/com/logistics/platform/auth_service/common/exception/CustomExceptionHandler.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +19,13 @@ public class CustomExceptionHandler {
     public ResponseEntity<?> apiException(CustomApiException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleValidationException(BindingResult bindingResult) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new ResponseDto<>(ResponseDto.FAILURE,
+                bindingResult.getAllErrors().get(0).getDefaultMessage(),
+                HttpStatus.BAD_REQUEST));
     }
 }
