24fd46ea9cd7fe76a9330bb227a0257dd3d90201
yurim <y1551463@gmail.com>
2024-12-13 15:39:47 +0900

    fix : 헤더 role과 현재 role 검증

--- auth-service/src/main/java/com/logistics/platform/auth_service/config/jwt/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/com/logistics/platform/auth_service/config/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.logistics.platform.auth_service.config.jwt;
 
+import com.logistics.platform.auth_service.application.dto.CustomUserDetails;
 import com.logistics.platform.auth_service.application.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,11 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -22,10 +26,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         FilterChain filterChain) throws ServletException, IOException {
         String username = request.getHeader("X-User-Name");
         String role = request.getHeader("X-User-Role");
-        System.out.println(username + role);
+        log.info("username {}, role {}", username, role);
+
 
         if (username != null && role != null) {
-            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+            CustomUserDetails userDetails = (CustomUserDetails) customUserDetailsService.loadUserByUsername(username);
+
+            if(!role.equals(userDetails.getRole())) {
+                log.info("userDetails role {}", userDetails.getRole());
+                response.setStatus(HttpStatus.FORBIDDEN.value()); // 403 상태 코드
+                response.getWriter().write("Role 정보가 일치하지 않습니다");
+                return;
+            }
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                 userDetails, null, userDetails.getAuthorities());
