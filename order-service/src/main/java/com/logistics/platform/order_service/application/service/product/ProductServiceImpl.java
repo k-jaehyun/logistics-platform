13f36adc0d1c9504278c8d03896f21a16cfcee52
k-jaehyun <wogus146@gmail.com>
2024-12-17 02:45:04 +0900

    fix: OrderService getProduct 캐싱 삭제

--- order-service/src/main/java/com/logistics/platform/order_service/application/service/product/ProductServiceImpl.java
+++ order-service/src/main/java/com/logistics/platform/order_service/application/service/product/ProductServiceImpl.java
@@ -4,7 +4,6 @@ import com.logistics.platform.order_service.application.dto.ProductResponseDto;
 import com.logistics.platform.order_service.infrastructure.client.ProductClient;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +13,8 @@ public class ProductServiceImpl implements ProductService {
   private final ProductClient productClient;
 
   @Override
-  @Cacheable(value = "getProductCache", key = "#productId")
-  // TODO product 정보를 가져온 것은 수정하기 위함이라서 캐시가 필요 없을듯
-  public ProductResponseDto getProductDtoByProductId(UUID productId, String userName, String userRole) {
+  public ProductResponseDto getProductDtoByProductId(UUID productId, String userName,
+      String userRole) {
     return productClient.getProductDtoByProductId(productId, userName, userRole);
   }
 
