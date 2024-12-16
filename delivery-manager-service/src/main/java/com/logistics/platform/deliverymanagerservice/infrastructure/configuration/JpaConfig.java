4463ac44154da5dfafa6cccf380d84cac16b145a
hiimsajo <hiimsajo@gmail.com>
2024-12-17 06:51:55 +0900

    🩹 fix : 감사로그 자동으로 남도록 관련 추가

--- delivery-manager-service/src/main/java/com/logistics/platform/deliverymanagerservice/infrastructure/configuration/JpaConfig.java
+++ delivery-manager-service/src/main/java/com/logistics/platform/deliverymanagerservice/infrastructure/configuration/JpaConfig.java
@@ -3,9 +3,13 @@ package com.logistics.platform.deliverymanagerservice.infrastructure.configurati
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableJpaAuditing
@@ -16,6 +20,17 @@ public class JpaConfig {
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
     return new JPAQueryFactory(entityManager);
+  }
+
+  @Bean
+  public AuditorAware<String> auditorProvider() {
+    return () -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication == null || !authentication.isAuthenticated()) {
+        return Optional.of("Unknown");
+      }
+      return Optional.of(authentication.getName());  // 현재 유저 이름 반환
+    };
   }
 
 }
--- delivery-service/src/main/java/com/logistics/platform/delivery_service/deliveryRoute/infrastructure/configuration/JpaConfig.java
+++ delivery-service/src/main/java/com/logistics/platform/delivery_service/deliveryRoute/infrastructure/configuration/JpaConfig.java
@@ -1,5 +1,33 @@
 package com.logistics.platform.delivery_service.deliveryRoute.infrastructure.configuration;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class JpaConfig {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
+
+  @Bean
+  public AuditorAware<String> auditorProvider() {
+    return () -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication == null || !authentication.isAuthenticated()) {
+        return Optional.of("Unknown");
+      }
+      return Optional.of(authentication.getName());  // 현재 유저 이름 반환
+    };
+  }
 
 }
