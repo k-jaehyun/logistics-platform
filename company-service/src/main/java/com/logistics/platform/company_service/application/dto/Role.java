38841c4b559c7a38cd9a5d0f1e2ca961ef831dae
drinkgalaxy <drinkgalaxy1226@gmail.com>
2024-12-17 15:46:01 +0900

    ✨ feat : 실패 시 재시도 로직 추가

---
+++ company-service/src/main/java/com/logistics/platform/company_service/application/dto/Role.java
@@ -0,0 +1,19 @@
+package com.logistics.platform.company_service.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+  MASTER("ROLE_MASTER"),
+  HUB_MANAGER("ROLE_HUB_MANAGER"),
+  DELIVERY_MANAGER("ROLE_DELIVERY_MANAGER"),
+  COMPANY_MANAGER("ROLE_COMPANY_MANAGER");
+
+
+  private final String role;
+
+  Role(String role) {
+    this.role =role;
+  }
+
+}
